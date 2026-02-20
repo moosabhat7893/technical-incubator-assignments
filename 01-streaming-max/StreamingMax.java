ed29283d33c6205481b688fd4cd5b870f0723c3f
moosabhat7893 <moosabhat7893@gmail.com>
2026-02-21 00:16:01 +0530

    Clean up StreamingMax.java by removing comments
    Removed comments and unnecessary boilerplate code.

--- 01-streaming-max/StreamingMax.java
+++ 01-streaming-max/StreamingMax.java
@@ -1,14 +1,7 @@
-// Online Java Compiler
-// Use this editor to write, compile and run your Java code online
+
 
 import java.util.*;
 
-/*
-Streaming Max Analytics
-Find maximum in every sliding window of size k
-Time Complexity: O(N)
-Space Complexity: O(K)
-*/
 
 public class StreamingMax {
 
